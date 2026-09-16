ed0ba577d3355d88da6587752412436afea028c4
null <dulcealvarezperalta@westernwyoming.edu>
2026-09-16 18:49:08 +0000

    Declared variables.

--- Assignments/helloWorld.js
+++ Assignments/helloWorld.js
@@ -7,3 +7,9 @@
 let hello = "Hello World";
 
 console.log(hello);
+
+let greeting = "Hi, the program is running successfully!";
+let decimalNumber = 4.5;
+let zero = 0;
+let negativeNumber = -10
+let isTrue = true;
