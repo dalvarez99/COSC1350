5969b78bcbb6fc53e67ff3673e22e5c893be2231
null <dulcealvarezperalta@westernwyoming.edu>
2026-09-17 04:18:28 +0000

    ran some statements.

--- Assignments/helloWorld.js
+++ Assignments/helloWorld.js
@@ -13,3 +13,18 @@ let decimalNumber = 4.5;
 let zero = 0;
 let negativeNumber = -10
 let isTrue = true;
+
+console.log(typeof greeting);
+console.log(typeof decimalNumber);
+console.log(typeof zero);
+console.log(typeof negativeNumber);
+console.log(typeof isTrue);
+
+console.log ("Hi!\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
